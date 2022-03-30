13205241c83fa6aa9e11d43cb20fab6fbdd6f176
LHcouto <xluisx_couto@hotmail.com>
2022-03-29 22:51:49 -0300

    commit css

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,5 @@ const LojaController = require('../controllers/LojaController')
 
 routes.get('/', LojaController.getAll);
 routes.get('/cadastro', LojaController.cadastro);
-routes.post('create', LojaController.create);
+routes.post('/create', LojaController.create);
 module.exports = routes;
