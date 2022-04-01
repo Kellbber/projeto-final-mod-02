e538d8f10f8b9d1d612766a708d40a6d8fc491d5
Kellbber <kellbberbarcarolo@gmail.com>
2022-04-01 19:05:26 -0300

    subindo config

--- src/controllers/LojaController.js
+++ src/controllers/LojaController.js
@@ -1,82 +1,78 @@
-const Items = require("../models/Items")
-const orderById= {order: [["id","ASC"]]}
+const Items = require("../models/Items");
+const orderById = { order: [["id", "ASC"]] };
 const getAll = async (req, res) => {
-	try{
-	const loja = await Items.findAll(orderById);
-	res.render("index", {loja, itemPut: null, itemDel: null})
-	}catch (err) {
-	res.status(500).send({err: err.message})
-	}
-}
-const cadastro = (req, res) =>{
-    try{ 
-
-        res.render("cadastro");
-        }catch (err) {
-        res.status(500).send({err: err.message})
-        }
-}
+  try {
+    const loja = await Items.findAll(orderById);
+    res.render("index", { loja, itemPut: null, itemDel: null });
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+const cadastro = (req, res) => {
+  try {
+    res.render("cadastro");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
 const create = async (req, res) => {
-    try{ 
-        const item = req.body;
-        if(!item){
-            return res.redirect('cadastro')
-        }await Items.create(item)
-        res.redirect('/');
-        }catch (err) {
-        res.status(500).send({err: err.message})
-        }
-}
-    const detalhes = async (req, res) => {
-        const item = await Items.findByPk(req.params.id);
-        try{
-            res.render('detalhes',{ items: item, itemPut: null, itemDel: null});
-        }catch (err) {
-            res.status(500).send({err: err.message})
-            }
+  try {
+    const item = req.body;
+    if (!item) {
+      return res.redirect("cadastro");
     }
-    const getById = async (req, res) => {
-        try{
-            const method = req.params.method;
-            const loja = await Items.findAll();
-            const item = await Items.findByPk(req.params.id)
-    if(method == 'put'){
-    res.render("detalhes",{
-    items: item,
-    itemPut: item,
-    itemDel: null
-    
-     })
-    }else {
-    res.render('detalhes',{
-    loja,
-    itemPut: null,
-    itemDel: item
-    
-     })
-    
+    await Items.create(item);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+const detalhes = async (req, res) => {
+  const item = await Items.findByPk(req.params.id);
+  try {
+    res.render("detalhes", { items: item, itemPut: null, itemDel: null });
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+const getById = async (req, res) => {
+  try {
+    const method = req.params.method;
+    const loja = await Items.findAll();
+    const item = await Items.findByPk(req.params.id);
+    if (method == "put") {
+      res.render("detalhes", {
+        items: item,
+        itemPut: item,
+        itemDel: null,
+      });
+    } else {
+      res.render("detalhes", {
+        loja,
+        itemPut: null,
+        itemDel: item,
+      });
     }
-     }catch (err) {
-            res.status(500).send({err: err.message});
-    		         }
-    };
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
 
-    const update = async (req,res) => {
-        try{
-            const item = req.body;
-            await Items.update(item, {where: {id: req.params.id}})
-            res.redirect("/")
-                 }catch (err) {
-            res.status(500).send({err: err.message});
-    		         }
-    }
-    
+const update = async (req, res) => {
+  try {
+    const item = req.body;
+    await Items.update(item, { where: { id: req.params.id } });
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
 
-module.exports = { 
-    cadastro,
-	getAll,
-    create,
-    detalhes,
-    getById,
-    update,
-	}
+module.exports = {
+  cadastro,
+  getAll,
+  create,
+  detalhes,
+  getById,
+  update,
+};
